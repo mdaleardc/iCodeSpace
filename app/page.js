8a29df1508fd010e12b4a3e59e736debece12380
mdaleardc <ismailaliamot@gmail.com>
2025-09-28 16:44:11 +0600

    push test

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-green-500 text-white font-bold text-5xl text-center pt-24">
+    <div className="w-screen h-screen bg-indigo-500 text-white font-bold text-5xl text-center pt-24">
     Hello, iCodeSpace
     </div>
   );
