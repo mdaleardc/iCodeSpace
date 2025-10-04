c3bf551a0fff70cb507de40a28d8b741020d97ec
mohammedmur-bit <mohammedmur350@gmail.com>
2025-10-04 14:01:53 +0600

    client section complete

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 import Hero from "@/components/Hero"
 import Grid from "@/components/ui/grid";
 import RecentProjects from "@/components/RecentProjects";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
 
 export default function Home() {
   return (
@@ -9,6 +12,9 @@ export default function Home() {
     <Hero />
     <Grid />
     <RecentProjects />
+    <Clients />
+    <Experience />
+    <Approach />
     </div>
     </main>
   );
